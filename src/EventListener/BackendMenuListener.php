e9d25db066a46a5a04bc95991f720b02aa5aad07
saschawustmann <an@saschawustmann.de>
2020-10-22 17:11:25 +0200

    Updated the messages and added the new theme

--- src/EventListener/BackendMenuListener.php
+++ src/EventListener/BackendMenuListener.php
@@ -2,6 +2,8 @@
 
 namespace Heartbits\ContaoBackendTheme\EventListener;
 
+use Contao\System;
+use Contao\Backend;
 use Contao\BackendUser;
 use Contao\CoreBundle\Event\MenuEvent;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -21,57 +23,61 @@ class BackendMenuListener
 
     public function onBuild(MenuEvent $event): void
     {
-        $objUser = BackendUser::getInstance();
-        $factory = $event->getFactory();
-        $tree = $event->getTree();
+        $theme = Backend::getTheme();
+        if ($theme === 'heart-bits' || $theme === 'saschawustmann') {
+            $objUser = BackendUser::getInstance();
+            $factory = $event->getFactory();
+            $tree = $event->getTree();
 
-        if ('headerMenu' !== $tree->getName() ) {
-            return;
+            if ('headerMenu' !== $tree->getName() ) {
+                return;
+            }
+
+            $support = $factory
+                ->createItem('support')
+                ->setLabel($GLOBALS['TL_LANG'][$theme]['need_help'])
+                ->setAttribute('class', 'submenu support')
+                ->setLabelAttribute('class', 'h2')
+            ;
+
+            $tree->addChild($support);
+
+            $name = $factory
+                ->createItem('info')
+                ->setLabel('<strong>' . $GLOBALS['TL_LANG'][$theme]['title'] . '</strong>' . $GLOBALS['TL_LANG'][$theme]['phone'])
+                ->setAttribute('class', 'info')
+                ->setExtra('safe_label', true)
+            ;
+
+            $support->addChild($name);
+
+            $website = $factory
+                ->createItem('website')
+                ->setLabel($GLOBALS['TL_LANG'][$theme]['website'])
+                ->setUri($GLOBALS['TL_LANG'][$theme]['website_link'])
+                ->setLinkAttribute('class', 'icon-link')
+                ->setExtra('safe_label', true)
+            ;
+
+            $support->addChild($website);
+
+            $email = $factory
+                ->createItem('email')
+                ->setLabel($GLOBALS['TL_LANG'][$theme]['send_email'])
+                ->setUri($GLOBALS['TL_LANG'][$theme]['email'])
+                ->setLinkAttribute('class', 'icon-email')
+                ->setExtra('safe_label', true)
+            ;
+
+            $support->addChild($email);
+
+            if ($objUser->isAdmin && System::getContainer()->getParameter('kernel.environment') == 'dev') {
+                $tree->reorderChildren(['support', 'alerts', 'preview', 'submenu', 'burger']);
+            } elseif ($objUser->isAdmin) {
+                $tree->reorderChildren(['support', 'alerts', 'debug', 'preview', 'submenu', 'burger']);
+            } else {
+                $tree->reorderChildren(['support', 'alerts', 'preview', 'submenu', 'burger']);
+            }
         }
-
-        $support = $factory
-            ->createItem('support')
-            ->setLabel($GLOBALS['TL_LANG']['heartbits']['need_help'])
-            ->setAttribute('class', 'submenu support')
-            ->setLabelAttribute('class', 'h2')
-        ;
-
-        $tree->addChild($support);
-
-        $name = $factory
-            ->createItem('info')
-            ->setLabel('<strong>heart-bits</strong>+49 2261 9158002')
-            ->setAttribute('class', 'info')
-            ->setExtra('safe_label', true)
-        ;
-
-        $support->addChild($name);
-
-        $website = $factory
-            ->createItem('website')
-            ->setLabel($GLOBALS['TL_LANG']['heartbits']['website'])
-            ->setUri('https://www.heart-bits.com/')
-            ->setLinkAttribute('class', 'icon-link')
-            ->setExtra('safe_label', true)
-        ;
-
-        $support->addChild($website);
-
-        $email = $factory
-            ->createItem('email')
-            ->setLabel($GLOBALS['TL_LANG']['heartbits']['send_email'])
-            ->setUri('mailto:hi@heart-bits.com')
-            ->setLinkAttribute('class', 'icon-email')
-            ->setExtra('safe_label', true)
-        ;
-
-        $support->addChild($email);
-
-        if ($objUser->isAdmin) {
-            $tree->reorderChildren(['support', 'alerts', 'debug', 'preview', 'submenu', 'burger']);
-        } else {
-            $tree->reorderChildren(['support', 'alerts', 'preview', 'submenu', 'burger']);
-        }
-
     }
 }
